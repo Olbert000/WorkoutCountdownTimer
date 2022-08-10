de611f1d951d7c0164d34a3b7fabee4ce43b1e8b
Olbert000 <oliverkerr@gmail.com>
2022-08-10 16:07:43 +1000

    Initial Commit

---
+++ WorkoutCountdownTimer/WorkoutCountdownTimerApp.swift
@@ -0,0 +1,20 @@
+//
+//  WorkoutCountdownTimerApp.swift
+//  WorkoutCountdownTimer
+//
+//  Created by Oliver Kerr on 10/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutCountdownTimerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
